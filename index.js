ebdd6d09a09693b92a03f5c5f625c12aafe2caac
Brian Muriithi M <brianmuriithi71@gmail.com>
2022-12-08 09:19:30 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const name = ["Guadalupe","Ollie","Aki"];
+function writeCards(name,gift = "surprise"){
+    let array = [];
+    for (let i = 0; i < name.length; i++){
+        array.push (`Thank you, ${name[i]}, for the wonderful ${gift} gift!`);
+    }
+    return array;
+}
+let num = [0,1,2,3,4,5,6,7,8,9,10]
+const countDown = (num) => {
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
